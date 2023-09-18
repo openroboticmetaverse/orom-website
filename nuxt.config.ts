c0ed6dc746fd5fc21cd61859a99da112735af3f2
parhasard <hazar_karadag@hotmail.com>
2023-09-18 17:47:23 +0200

    add static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  target: 'static',
   head: {title:"Nuxt Template"},
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
