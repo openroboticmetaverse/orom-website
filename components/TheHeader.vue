543e7430e6fdfb484ffb6dc5d2d434e5e508a173
parhasard <hazar_karadag@hotmail.com>
2023-09-19 22:33:45 +0200

    add fallback styling

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,7 +4,7 @@
   <video v-if="!videoFailed" alt="" class=" absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" id="video" autoplay loop muted playsinline>
       <source src="/pages/home/automation.mp4" type="video/mp4">
     </video>
-    <img v-else src="https://images.unsplash.com/photo-1567789884554-0b844b597180?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="Fallback Image">
+    <img v-else class=" absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center" src="https://images.unsplash.com/photo-1567789884554-0b844b597180?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="Fallback Image">
   <div class="video-overlay"></div>
   <div class="overlay-bottom"></div>
       <div class="overlay-top"></div>
