4d6030b840d920fbc9a12d0aa62c7eb26e9c7d64
parhasard <hazar_karadag@hotmail.com>
2023-09-21 00:17:56 +0200

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss',
+  ],
   target: 'static',
+  buildModules: [
+    '@nuxtjs/pwa',
+  ],
   pwa: {
     icon: {
-      source:'./public/icon.png'
+      source: './public/icon.png'
     }
   },
   app: {
