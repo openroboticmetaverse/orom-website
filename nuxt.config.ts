3c5d5e38fbf11ab292682981fc07f69ee7385253
parhasard <hazar_karadag@hotmail.com>
2023-09-20 23:23:50 +0200

    add favicon package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,17 +8,22 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: "Roboverse",
-       htmlAttrs: {
+      htmlAttrs: {
         lang: 'en'
       },
       meta: [
-        {
-          name: 'description', content: 'A Nuxt 3 template with Tailwind CSS'
-        }
+        { name: 'description', content: 'A Nuxt 3 template with Tailwind CSS' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' }
       ],
       link: [
-        { rel: 'icon', sizes: '180x180', href: '/icon.png'}
-      ]
+        // ...other link tags
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      ],
     },
 
 
