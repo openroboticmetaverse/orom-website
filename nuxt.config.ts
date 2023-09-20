078f15f236fde141534d0656bfd911fb5d79f26e
parhasard <hazar_karadag@hotmail.com>
2023-09-21 00:55:01 +0200

    change config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,18 +22,19 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'A Nuxt 3 template with Tailwind CSS' },
         { name: 'msapplication-TileColor', content: '#da532c' },
-        { name: 'theme-color', content: '#000000' }
+        { name: 'theme-color', content: '#000000' },
+        {
+          name: 'apple-mobile-web-app-capable',
+          content: 'yes'
+        },
+        {
+          name: 'apple-mobile-web-app-status-bar-style',
+          content: 'black-translucent'
+        },
       ],
       link: [
         { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'apple-touch-icon', sizes:'180x180', href: '/apple-touch-icon.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-touch-icon-72x72.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-touch-icon-144x144.png?v=0jfosdjpf' },
-        { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png?v=0jfosdjpf' },
+        { rel: 'apple-touch-icon', type: 'image/png', sizes:'180x180', href: '/apple-touch-icon.png?v=0jfosdjpf' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'manifest', href: '/site.webmanifest' },
