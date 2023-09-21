e9977d4910faa9607491ea1f103792e8eb4af84d
parhasard <hazar_karadag@hotmail.com>
2023-09-21 13:30:27 +0200

    iphone notch trial 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
         lang: 'en'
       },
       meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
         { name: 'apple-mobile-web-app-title', content: "Roboverse" },
