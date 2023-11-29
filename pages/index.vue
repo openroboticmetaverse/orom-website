6b30def52cf3141e5d6f1aca442c58c932538522
parhasard <hazar_karadag@hotmail.com>
2023-11-29 12:45:09 +0100

    network gradient

--- pages/index.vue
+++ pages/index.vue
@@ -79,7 +79,7 @@
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
           <div class="mx-auto max-w-8xl lg:mx-0">
 
-            <h2 class="text-4xl font-bold tracking-tight text-left text-white sm:text-6xl">Network</h2>
+            <h2 class="text-4xl font-bold tracking-tight gradient-text-left text-left text-white sm:text-6xl">Network</h2>
             <p class="mt-6 text-lg leading-8 ml-200 max-w-2xl text-left text-gray-300">Our goal is to cultivate a
               community where robotics enthusiasts can meet, collaborate on projects, and share insights, enhancing
               partnerships and idea exchange.</p>
