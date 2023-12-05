dca3ef0423425622dbaeab7c9289421325f10f93
parhasard <hazar_karadag@hotmail.com>
2023-12-05 21:57:24 +0100

    reduce font size

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,7 +7,7 @@
         Reach us!
       </template>
       <template v-slot:description>
-                <p class="mt-6 text-2xl sm:text-6xl leading-8 ml-200 max-w-2xl text-left text-gray-300">Ready to Make a Difference? Reach out to us and start your journey today!</p>
+                <p class="mt-6 text-xl sm:text-6xl leading-8 ml-200 max-w-2xl text-left text-gray-300">Ready to Make a Difference? Reach out to us and start your journey today!</p>
                 <div class="py-10"></div>
                  
             </template>
