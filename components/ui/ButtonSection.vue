7fc56bf4324e1d300a6f5dd7132bbb3d71203e37
parhasard <hazar_karadag@hotmail.com>
2023-12-05 18:06:51 +0100

    add margin to buttons

--- components/ui/ButtonSection.vue
+++ components/ui/ButtonSection.vue
@@ -1,25 +1,19 @@
 <template>
-
-
-<section>
-    <section class="sticky">
-
-
-      <div
-        class="max-w-lg px-4 pb-24 mx-auto text-left md:max-w-none md:text-center"
-      >
-        <div class='text-center py-4 space-x-4'>
-
-          <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-[#192033] border border-[#E2E8F0] translate-y-1 text-white hover:bg-neutral-500 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
-            <span> Explore opportunities</span>
-          </button>
-
-          <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-white border border-[#E2E8F0] translate-y-1 text-[#16161d] hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
-            <span> Example Projects </span>
-          </button>
+    <section>
+      <section class="sticky">
+        <div class="max-w-lg px-4 pb-24 mx-auto text-left md:max-w-none md:text-center">
+          <div class='text-center py-4 space-x-4 space-y-4 md:space-y-0'>
+  
+            <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-[#192033] border border-[#E2E8F0] translate-y-1 text-white hover:bg-neutral-500 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
+              <span> Explore opportunities</span>
+            </button>
+  
+            <button class="backdrop-blur-sm transition duration-500 ease-in-out bg-white border border-[#E2E8F0] translate-y-1 text-[#16161d] hover:bg-neutral-200 text-lg font-semibold py-3 px-6 rounded-3xl inline-flex items-center">
+              <span> Example Projects </span>
+            </button>
+          </div>
         </div>
-      </div>
-
+      </section>
     </section>
-  </section>
-</template>
+  </template>
+  
