783c88965c82c942503fa7a71a030d0e857229cf
parhasard <hazar_karadag@hotmail.com>
2023-12-07 19:55:04 +0100

    remove pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss','@pinia/nuxt',
+  modules: ['@nuxtjs/tailwindcss',
   ],
   target: 'static',
   app: {
