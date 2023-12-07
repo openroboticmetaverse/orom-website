33f8713a81919ca6dfac318ee91b9b24a507ab0a
parhasard <hazar_karadag@hotmail.com>
2023-12-07 19:24:54 +0100

    uncomment

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
         <div>
             <UiEmptySection>
       <template v-slot:mainTitle2>
@@ -52,11 +52,4 @@ useHead({
       }]
 })
 
-</script> -->
-
-<template>
-
-</template>
-
-<script>
 </script>
