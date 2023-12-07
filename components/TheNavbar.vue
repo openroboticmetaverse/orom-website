4d7ebd658a829d2d56be551054d7b596c23332e8
parhasard <hazar_karadag@hotmail.com>
2023-12-07 19:13:43 +0100

    change card

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -6,7 +6,7 @@
                     <div class="absolute inset-y-0 right-0 flex items-center sm:hidden ">
                         <!-- Mobile menu button-->
                         <button type="button" @click="toggleMenu" ref="menuButton"
-                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:underline hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                             aria-controls="mobile-menu" aria-expanded="false">
                             <span class="absolute -inset-0.5"></span>
                             <span class="sr-only">Open main menu</span>
