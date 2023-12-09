eac7723562d735a2f8d0a53c3603018f62747d30
parhasard <hazar_karadag@hotmail.com>
2023-12-09 15:09:30 +0100

    add to project head

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -10,12 +10,16 @@ const route = useRoute();
 const projectsStore = useProjectsStore();
 const { projects } = storeToRefs(projectsStore);
 
+
 const projectId = route.params.id;
 
 const project = computed(() => {
   return projectsStore.projects.find((p) => p.id === projectId);
 });
-//  fetch the products
+
+const baseUrl = 'https://www.openroboticmetaverse.org';
+const fullImageUrl = project.value ? `${baseUrl}${project.value.image}` : '';
+
 useHead({
   title: `${project.value.title} | open robotic metaverse `,
   meta: [
@@ -23,6 +27,26 @@ useHead({
       name: "description",
       content: `${project.value.title} | Open Robotic Metaverse`,
     },
+        {
+      property: "og:title",
+      content: `${project.value.title} | Open Robotic Metaverse`,
+    },
+    {
+      property: "og:description",
+      content: `${project.value?.idea[1]}`, 
+    },
+    {
+      property: "og:image",
+      content: fullImageUrl, 
+    },
+    {
+      property: "og:image:width",
+      content: "1200",
+    },
+    {
+      property: "og:image:height",
+      content: "630", 
+    }
   ],
 });
 
