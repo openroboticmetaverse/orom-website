e8faa304d0127ebd87b0970155bcc8bd8c95271d
Karim Siala <karimsiala97@gmail.com>
2023-12-14 09:47:12 +0100

    Refactor imprint page layout and content

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,50 +1,49 @@
 <template>
-
-<div class="container ">
-        <header>
-            <h1>Open Robotic Metaverse e.V.</h1>
+    <div class="container mx-auto px-12">
+        <header class="text-center py-4">
+            <h1 class="text-4xl font-bold gradient-text-right">Imprint</h1>
         </header>
 
-        <section>
-            <h2>Information in Accordance with Section §5 TMG</h2>
-            <address>
+        <section class="py-4">
+            <h2 class="text-2xl font-semibold uppercase gradient-text-left">Information in Accordance with Section §5 TMG</h2>
+            <address class="mt-2 text-lg">
+                open robotic metaverse e.V.<br>
                 Jülicher Str. 209d<br>
-                52070 Aachen
+                52070 Aachen<br>
+                Germany
             </address>
         </section>
 
-        <section class="contact">
-            <h3>Represented By</h3>
-            <ul>
-                <li>Mr. Amine Kharrat</li>
-                <li>Mr. Karim Siala</li>
-                <li>Mr. Jan Strehl</li>
+        <section class="py-4">
+            <h3 class="text-2xl font-semibold uppercase gradient-text-left">Represented By</h3>
+            <ul class="list-inside">
+                <li>Mr. <b>Amine Kharrat</b></li>
+                <li>Mr. <b>Karim Siala</b></li>
+                <li>Mr. <b>Jan Strehl</b></li>
             </ul>
+        </section>
+        <section class="py-4">
 
-            <h3>Contact</h3>
-            <p>E-Mail: <a href="mailto:openroboticmetaverse@gmail.com">openroboticmetaverse@gmail.com</a></p>
-            <p>Internet address: <a href="http://www.openroboticmetaverse.org">www.openroboticmetaverse.org</a></p>
+            <h3 class="text-2xl font-semibold uppercase gradient-text-left">Contact</h3>
+            <p class="mt-2 text-lg">E-Mail: <a href="mailto:openroboticmetaverse@gmail.com"
+                    class="text-blue-500 underline">openroboticmetaverse@gmail.com</a></p>
+            <p class="mt-2 text-lg">Internet address: <a href="http://www.openroboticmetaverse.org"
+                    class="text-blue-500 underline">www.openroboticmetaverse.org</a></p>
         </section>
 
-        <section class="register">
-            <h3>Register Entry</h3>
-            <p>Entry in Vereinsregister (register of associations)</p>
-            <p>Register Number: VR 5554<br>Register Court: Amtsgericht Aachen</p>
-        </section>
+        <section class="py-4">
+    <h3 class="text-2xl font-semibold uppercase gradient-text-left">Disclaimer</h3>
 
-        <section class="disclaimer">
-            <h3>Disclaimer</h3>
-            <h4>Accountability for content</h4>
-            <p>The contents of our pages have been created with the utmost care...</p>
+    <h4 class="text-xl font-bold mt-2">Accountability for content</h4>
+    <p class="mt-2 text-lg">The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents' accuracy, completeness or topicality. According to statutory provisions, we are furthermore responsible for our own content on these web pages. In this context, please note that we are accordingly not obliged to monitor merely the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity. Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).</p>
 
-            <h4>Accountability for links</h4>
-            <p>Responsibility for the content of external links...</p>
+    <h4 class="text-xl font-bold mt-2">Accountability for links</h4>
+    <p class="mt-2 text-lg">Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective link immediately.</p>
 
-            <h4>Copyright</h4>
-            <p>Our web pages and their contents are subject to German copyright law...</p>
-        </section>
-    </div>
-
+    <h4 class="text-xl font-bold mt-2">Copyright</h4>
+    <p class="mt-2 text-lg">Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law (§ 44a et seq. of the copyright law), every form of utilizing, reproducing or processing works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights. Individual reproductions of a work are allowed only for private use, so must not serve either directly or indirectly for earnings. Unauthorized utilization of copyrighted works is punishable (§ 106 of the copyright law).</p>
+  </section>
+</div>
 </template>
 
 
