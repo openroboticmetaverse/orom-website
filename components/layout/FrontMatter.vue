c91e0cbea8d2b2cac843f9a96dc44580d4d54cbc
amine <amine.kharrat@rwth-aachen.de>
2024-01-04 02:51:39 +0100

    text changes

--- components/layout/FrontMatter.vue
+++ components/layout/FrontMatter.vue
@@ -42,12 +42,12 @@
           </div>
           <div class="flex flex-col-reverse sliding-text-5">
             <dt class="text-base sm:text-xl leading-7 text-gray-300">
-              Building
+              Supported by
             </dt>
             <dd
               class="text-xl sm:text-2xl font-bold leading-9 tracking-tight text-white"
             >
-              A metaverse
+              the metaverse
             </dd>
           </div>
         </dl>
